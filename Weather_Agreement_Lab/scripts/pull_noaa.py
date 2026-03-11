855cbd27f74018e9cfa01f64e861a62eda194a50
Jeremy Evert on Rocky 9.7 Maise <jeremy.evert@swosu.edu>
2026-03-11 15:01:13 -0500

    cleaned up scripts.

--- Weather_Agreement_Lab/scripts/pull_noaa.py
+++ Weather_Agreement_Lab/scripts/pull_noaa.py
@@ -1,5 +1,29 @@
 import os
+from datetime import datetime
 import requests
 import pandas as pd
-from datetime import datetime
+from pathlib import Path
+from dotenv import load_dotenv
+
+from scripts.config import DATA_RAW, ensure_directories
+
+
+def download_noaa_hourly(station_id: str, year: int):
+
+    ensure_directories()
+
+    url = f"https://www.ncei.noaa.gov/data/global-hourly/access/{year}/{station_id}.csv"
+
+    output_file = DATA_RAW / f"{station_id}_{year}.csv"
+
+    response = requests.get(url)
+
+    response.raise_for_status()
+
+    with open(output_file, "wb") as f:
+        f.write(response.content)
+
+    print("Saved:", output_file)
+
+    return output_file
 
